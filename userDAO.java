6b2313f08188560c74757ad5d5da4c369cc79772
mounika-ammu <101688297+mounika-ammu@users.noreply.github.com>
2022-03-29 13:34:18 +0530

    Create userDAO.java

---
+++ userDAO.java
@@ -0,0 +1,15 @@
+# onlineshopping
+package com.model;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface UserDAO {
+	List<User> getAllUsers();
+	void registerUser(User user);
+	boolean deleteUser(int id);
+	User findUser(int id);
+
+}
